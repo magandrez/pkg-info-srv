ebe28d887d403788bd74d3b8bb9ccc31c30bc8d0
Manuel González <magandrez@gmail.com>
2019-09-08 22:17:56 +0300

    Craft package parsing and response
    Quite handcrafted way of parsing packages using a dictionary.

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -10,11 +10,12 @@ class AppController < Sinatra::Base
   get '/api/packages/:package_name' do
     $fp.load_file_map
     arr = $fp.file_map
-    package_index = arr.select{ |package| package[0].include? params[:package_name] }.flatten
+    package_index = arr.select{ |package| package[0] == params[:package_name] }.flatten
     unless package_index.empty?
-      text = $fp.read_package(package_index[1], # Package start line in file
-                              arr[arr.index(package_index)+1][1]) # Package end line (the beginning of the next in the map)
-      text
+      content_type :json
+      # TODO: make the below call safe for EOF errors
+      info_hash = $fp.parse_text(package_index[1], # Package start line in file
+                                 arr[arr.index(package_index)+1][1]) # Package end line (the beginning of the next in the map)
     else
       halt 404, 'Package not found!'
     end
--- lib/file_parser.rb
+++ lib/file_parser.rb
@@ -1,4 +1,5 @@
 require 'rio'
+require 'json'
 
 class FileParser
 
@@ -29,6 +30,37 @@ class FileParser
 
   def read_package(from_line, to_line)
     rio(@file).lines[from_line...to_line]
+  end
+
+  def parse_text(from, to)
+    text = read_package(from, to)
+    text.map!{ |str| str.delete!("\n") }
+    result = {}
+    result[:name] = parse_name(text)
+    result[:description] = parse_description(text)
+    result[:depends_on] = parse_dependencies(text)
+    result.to_json
+  end
+
+  def parse_name(text)
+    text.select{ |line| /^Package/ =~ line }.first.gsub("Package: ", "")
+  end
+
+  def parse_description(text)
+    description = text.select{|item| item[0] == ' ' || item =~ /^Description/ }.map{|item| item.gsub(/^Description: /, '')}
+    description.join("\n")
+  end
+
+  def parse_dependencies(text)
+    dep = []
+    dep = text.select{ |line| /^Depends/ =~ line }.first
+    return [] if dep.nil?
+    dep.gsub!("Depends: ", "")
+    dep.gsub!(/\(.*?\)/, '').# Removes all versioning -- between parentheses
+      split(","). # Splits string into array of strings by comma
+      map! do |elem|
+        elem.split("|")[0].strip # Splits by | and takes the first part (i.e. orig dep vs alternative)
+      end.uniq # Don't repeat deps
   end
 
 end
